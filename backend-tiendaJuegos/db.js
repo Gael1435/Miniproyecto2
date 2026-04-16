378b57c6b2acd7c73f87def38f88875f7cded14c
Gael1435 <elirivera_2782@hotmail.com>
2026-04-15 23:04:46 -0600

    prueba de corregir el deployment

--- backend-tiendaJuegos/db.js
+++ backend-tiendaJuegos/db.js
@@ -1,19 +1,16 @@
 const mysql = require('mysql2');
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  port: process.env.DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-db.connect(err => {
-  if(err){
-    console.log("Error DB:", err);
-  }else{
-    console.log("Conectado a MySQL");
-  }
-});
+console.log("Pool MySQL listo");
 
 module.exports = db;
