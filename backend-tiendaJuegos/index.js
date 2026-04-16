2ae347881e55cec64635be209df892e53be741b4
Gael1435 <elirivera_2782@hotmail.com>
2026-04-15 21:14:18 -0600

    Cambiar el puerto para subir a render

--- backend-tiendaJuegos/index.js
+++ backend-tiendaJuegos/index.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+const PORT = process.env.PORT || 3000;
 // variables con las rutas
 const productosRoutes = require('./routes/productos.routes');
 const contactoRoutes = require('./routes/contactos.routes');
@@ -15,6 +15,6 @@ app.use('/comprar', comprarRoutes);
 app.use('/productos', productosRoutes);
 app.use('/contacto', contactoRoutes);
 
-app.listen(3000, () => {
-  console.log('Servidor corriendo en http://localhost:3000');
+app.listen(PORT, () => {
+  console.log('Servidor corriendo en ' + PORT);
 });
